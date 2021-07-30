ee99ddc553ad4fbbe654ba8a874528bf93ad3a86
Lilian <73485512+LysnakeIT@users.noreply.github.com>
2021-07-30 20:22:35 +0200

    Update wiki.js

--- commands/info/wiki.js
+++ commands/info/wiki.js
@@ -20,12 +20,22 @@ module.exports = {
                 redirects: '',
                 formatversion: 2
             });
-        if (query.length === 0) return message.channel.send("Vous devez mentionner une information à rechercher");
-        if (body.query.pages[0].missing) return message.channel.send("Pas de résultats, réessayer avec plus d'informations.");
+        if (query.length === 0) {
+            var erreur = new Discord.MessageEmbed()
+                .setColor("#2F3136")
+                .setTitle("<:warning:869206692091531305> Vous devez mentionner une information à rechercher !")
+            return message.channel.send(erreur);
+        }
+        if (body.query.pages[0].missing) {
+            var erreurInfo = new Discord.MessageEmbed()
+                .setColor("#2F3136")
+                .setTitle("<a:non:802645550435532810> Pas de résultats, réessayer avec plus d'informations.")
+            return message.channel.send(erreurInfo);
+        }
         const embed = new Discord.MessageEmbed()
             .setColor("00FF04")
             .setTitle(body.query.pages[0].title)
-            .setFooter('Developpé par Lysnake', 'https://cdn.discordapp.com/avatars/337210490453229579/a_f5e5111527a275d4fd335a89253291a4.gif?size=128')
+            .setFooter('Developpé par Lysnake', 'https://cdn.discordapp.com/avatars/337210490453229579/85a0a01303bcb266778cb0fab7d5808b.webp?size=256')
             .setTimestamp()
             .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Wikipedia-logo-v2-fr.svg/892px-Wikipedia-logo-v2-fr.svg.png')
             .setDescription(body.query.pages[0].extract.substr(0, 2000).replace(/[\n]/g, '\n\n'));
