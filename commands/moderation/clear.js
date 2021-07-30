3bad8977057903bf953b1b312c5da9576ede9fb9
Lilian <73485512+LysnakeIT@users.noreply.github.com>
2021-07-30 20:26:57 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -5,14 +5,17 @@ module.exports = {
     description: "Clear un channel",
     run: async (Client, message, args) => {
         message.delete();
+        var embedMention = new Discord.MessageEmbed()
+            .setColor("#2F3136")
+            .setTitle("<:warning:869206692091531305> Veuillez mentionner le nombre de messages")
         if (!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.channel.send("**Vous n'avez pas la permission d'éxécuter cette commande !**");
         let args2 = message.content.split(" ").slice(1);
-        if (!args2[0]) return message.channel.send("**Vous devez préciser le nombre de messages à supprimer !**")
+        if (!args2[0]) return message.channel.send(embedMention)
         message.channel.bulkDelete(args[0]).then(() => {
-        });
-        setTimeout(() => {
-            message.channel.send(`**${args2[0]} messages ont été supprimés !**`)
-            console.log(`${message.author.username} a supprimé ${args2[0]} messages`)
-        }, 500);
+            setTimeout(() => {
+                message.channel.send(`**${args2[0]} messages ont été supprimés !**`)
+                console.log(`${message.author.username} a supprimé ${args2[0]} messages`)
+            }, 500);
+        })
     }
 }
