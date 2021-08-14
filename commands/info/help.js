29c11aa142a0ccf6e781203b431c3acbfd8853b3
Lilian <73485512+LysnakeIT@users.noreply.github.com>
2021-08-14 22:54:40 +0200

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -8,8 +8,7 @@ module.exports = {
         const help_embed = new Discord.MessageEmbed()
             .setColor("#00FF04")
             .setTitle("**Kroma Help**")
-            .setDescription("**Préfixe :** `!`\n\n:desktop: Liste des commandes **hardware/Software** : \n\n`!nvidia` `!doc` `!opti` `!cg` `!cpu` `!ddu` `!revo` `!cleaninstall` \n\n `!reflex` `!fortnite` `!navigateur` `!diag` `!formatage` `!obs` \n\n `!config`\n\n :earth_africa: Liste des commandes **Servers** : \n\n `!serverinfos` `!info` `!covid` `!math` `!ping` `!pourcentage` `!weather`\n\n `!wiki` `!translate` `!system` \n\n :video_game: Liste des commandes **Games** : \n\n `!pendu` `!puissance4` `!quizz`")
+            .setDescription("**Préfixe :** `!`\n\n :earth_africa: Liste des commandes **Servers** : \n\n `!serverinfos` `!info` `!covid` `!math` `!ping` `!pourcentage` `!weather`\n\n `!wiki` `!translate` `!system` `!yt`\n\n :desktop: Commande **Hardware/Software** : \n\n`!hs`\n\n :video_game: Liste des commandes **Games** : \n\n `!pendu` `!puissance4` `!quizz`\n\n :headphones: Liste des commandes **Musique** : \n\n `!clear-queue` `!loop` `!play` `!queue` `!search` `!shuffle` `!skip` `!stop`")
         message.channel.send(help_embed)
-        console.log(`${message.author.tag} a utilisé la commande /help`)
     }
 }
